99e3ba022f5d71832a3e9b4f6d7605cc33ca595d
хорек2 <2@DESKTOP-DDH06GN>
2026-01-23 12:37:48 +0500

    Добавьте файлы проекта.

---
+++ pigFerm/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using pigFerm.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pigFerm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //lv1.ItemsSource = App.db.groupTypes.ToList();
+        }
+
+        private void employeeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new employeesPage());
+        }
+    }
+}
